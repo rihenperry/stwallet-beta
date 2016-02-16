cf960ef5b2be4b89d08f6799c5491be31d78dbdc
sudeep_dk <sudeep.makwana@gmail.com>
2016-02-16 12:00:13 +0530

    mongoose connection written in best code practice.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,44 @@
-// Packages
-var mongoose    = require('mongoose');
+// // Packages
+// var mongoose    = require('mongoose');
 
-// Connectivity To Mongo
-mongoose.connect('mongodb://localhost/wallet');
-var db = mongoose.connection;
+// // Connectivity To Mongo
+// mongoose.connect('mongodb://localhost/wallet');
+// var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Connection Error'));
+// db.on('error', console.error.bind(console, 'Connection Error'));
 
-db.once('open', function(){
-	console.log('Connected To Database');	
+// db.once('open', function(){
+// 	console.log('Connected To Database');	
+// });
+// Bring Mongoose into the app 
+var mongoose = require( 'mongoose' ); 
+
+// Build the connection string 
+var dbURI = 'mongodb://localhost/wallet'; 
+
+// Create the database connection 
+mongoose.connect(dbURI); 
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {  
+  console.log('Mongoose default connection open to ' + dbURI);
+}); 
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
 });
+
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', function() {  
+  mongoose.connection.close(function () { 
+    console.log('Mongoose default connection disconnected through app termination'); 
+    process.exit(0); 
+  }); 
+}); 
