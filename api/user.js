08c666d1d961561675481d0a88c83718047a0be1
Swanand <Swanand>
2016-02-17 18:05:06 +0530

    start with search.js

--- api/user.js
+++ api/user.js
@@ -4,7 +4,6 @@
 
 // Pages
 var userSchema      = require('../models/userSchema.js');       // User Schema
-var deviceSchema    = require('../models/deviceInfoSchema.js')  // DeviceInfo Schema
 var master          = require('../config/masterfunc.js');       // Master Functions
 var crypt           = require('../config/crypt.js');            // Crypt/Signature Related Functionality
 var mailer          = require('../config/mail.js');             // Mail Functionality
