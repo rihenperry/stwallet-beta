8f882238010689805db889555d34699a0d1d9a6e
prashant_tapase <prashant.bitstreet@gmail.com>
2016-02-25 14:02:58 +0530

    Made view folder and added index ejs for view table in node

--- bunyan_app/app.js
+++ bunyan_app/app.js
@@ -10,18 +10,7 @@ app.use(express.compress());
 // Serve up content from public directory
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
-
-
-
-
-
-
-//var array = obj.replace(/\}/g,'}, ');
-//var array = obj.replace(/\ /g,'');
-// var res   = array.split(", ");
-
-//console.log(array);
-
+app.set('view engine', 'ejs');  
 
 app.get('/', function(req, res){
 
@@ -35,45 +24,50 @@ app.get('/', function(req, res){
 	var length = array.length;
 	console.log(length);
 
+	//var data = JSON.parse(array);
+
 	//res.send(array);
 
-	var htmltable = '<table>'+
-						'<thead>'+
-							'<td>Number</td>'+
-							'<td>Name</td>'+
-							'<td>HostName</td>'+
-							'<td>PID</td>'+
-							'<td>Level</td>'+
-							'<td>Error</td>'+
-							'<td>Message</td>'+
-						'</thead>'+
-						'<tbody>';
-		var p = 1;
-		for(var i=0; i<length; i++ )
-		{
-			// console.log(array[i]);
-			var data = JSON.parse(array[i]);
+	// var htmltable = '<table>'+
+	// 					'<thead>'+
+	// 						'<td>Number</td>'+
+	// 						'<td>Name</td>'+
+	// 						'<td>HostName</td>'+
+	// 						'<td>PID</td>'+
+	// 						'<td>Level</td>'+
+	// 						'<td>Error</td>'+
+	// 						'<td>Message</td>'+
+	// 					'</thead>'+
+	// 					'<tbody>';
+	// 	var p = 1;
+	// 	for(var i=0; i<length; i++ )
+	// 	{
+	// 		// console.log(array[i]);
+	// 		var data = JSON.parse(array[i]);
 
-			htmltable += '<tr>'+
-							'<td>'+p+'</td>'+
-							'<td>'+data.name+'</td>'+
-							'<td>'+data.hostname+'</td>'+
-							'<td>'+data.pid+'</td>'+
-							'<td>'+data.level+'</td>'+
-							'<td>'+data.err+'</td>'+
-							'<td>'+data.msg+'</td>'+
-						 '</tr>';
+	// 		htmltable += '<tr>'+
+	// 						'<td>'+p+'</td>'+
+	// 						'<td>'+data.name+'</td>'+
+	// 						'<td>'+data.hostname+'</td>'+
+	// 						'<td>'+data.pid+'</td>'+
+	// 						'<td>'+data.level+'</td>'+
+	// 						'<td>'+data.err+'</td>'+
+	// 						'<td>'+data.msg+'</td>'+
+	// 					 '</tr>';
 
-			p++;
-		}
+	// 		p++;
+	// 	}
 					
-			htmltable	+=	'</tbody></table>';
+	//		htmltable	+=	'</tbody></table>';
 
-	res.send(htmltable);
+	// res.send(htmltable);
+	res.render('index', { data: array, length : length })
 
 })
 
-
+// app.get('/', function(req, res) {  
+//   res.render('index', { title: 'The index page!' })
+// });
 
 app.listen(process.env.PORT || 3010);
 console.log('Server started at port 3010');
