7e2bb542e32f67eb1cf9f8c8dc7bf2d3d09a0c84
swanand_searchtrade <swanand.searchtrade@gmail.com>
2016-03-10 18:59:54 +0530

    PHP email API saved data in notification server

--- api/mail.js
+++ api/mail.js
@@ -2,12 +2,13 @@
 /*jslint node: true */
 "use strict";
 
-var Mailgun     = require('mailgun-js');							// For Emails (Mailgun Module)
-var from_who    = 'donotreply@searchtrade.com';						// Sender of Email
-var api_key     = 'key-2b8f2419e616db09b1297ba51d7cc770';			// Api Key For Mailgun
-var domain      = 'searchtrade.com';								// Domain Name
-
-var mailgun     = new Mailgun({apiKey: api_key, domain: domain});	// Mailgun Object
+var notificationschema  = require('../model/notification_model.js');
+var Mailgun             = require('mailgun-js');							// For Emails (Mailgun Module)
+var from_who            = 'donotreply@searchtrade.com';						// Sender of Email
+var api_key             = 'key-2b8f2419e616db09b1297ba51d7cc770';			// Api Key For Mailgun
+var domain              = 'searchtrade.com';								// Domain Name
+    
+var mailgun             = new Mailgun({apiKey: api_key, domain: domain});	// Mailgun Object
 
 
 // Response Function
@@ -53,9 +54,16 @@ module.exports.sendmail = function (mailinfo, res) {
 // PHP Mail Functionality in Node
 module.exports.sendPHPmail = function (req, res){
     
-    var to      = req.body.to;
-    var subject = req.body.subject;
-    var message = req.body.message;
+    var to                  = req.body.to;
+    var subject             = req.body.subject;
+    var message             = req.body.message;
+    
+    var first_name          = req.body.first_name;
+    var last_name           = req.body.last_name;
+    var user_id             = req.body.user_id;
+    var notification_body   = req.body.notification_body;
+    
+    notification_body = first_name+" "+last_name+", "+notification_body;
     
     var mailOptions = {
 		from: 'Search Trade <donotreply@searchtrade.com>', 	// Sender address
@@ -78,9 +86,29 @@ module.exports.sendPHPmail = function (req, res){
         } else {
             
             console.log('Mail Sent Successfully');
-            sendResponse(req, res, 200, -1, "Success")
+            
+            var notification_message = new notificationschema({ 
+                user_id: user_id, 	                     // User Id
+                first_name: first_name, 				 // User First Name
+                last_name: last_name, 		             // User last Name
+                notification_body: notification_body,	 // Text
+            });
+            
+            notification_message.save(function(err){
+
+                if(err)
+                {
+                  console.log(err);
+                  return err;
+                }
+
+                console.log('Saved SuccessFully');
+                sendResponse(req, res, 200, -1, "Success");
+
+            });
+            
         }    
         
-    })
+    });
     
 }
