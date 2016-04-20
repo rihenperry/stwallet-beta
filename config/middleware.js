a38610edf1b5e1b67643b4679db719e1c4f3e4ba
rihbyne <rihen234@gmail.com>
2016-04-20 12:11:10 +0530

    middleware.js abstract common code which is required by 90 percent of app

---
+++ config/middleware.js
@@ -0,0 +1,58 @@
+var master = require('./masterfunc');
+var helpers = require('../helpers/utils');
+
+var checkPubSignKey = function(req, res, next) {
+  var pubKey = req.query.publicKey || req.body.publicKey;
+  log.info('public key middleware check->'+ pubKey);
+
+  var signature = req.query.signature || req.body.signature;
+  log.info('signature middleware check->'+ signature);
+
+  if(!master.validateParameter(pubKey, 'Public Key')) {
+    log.warn('public key error ->'+ pubKey);
+    master.sendResponse(req, res, 403, 1, "Mandatory field not found");
+    return;
+  } else if (!master.validateParameter(signature, 'Signature')) {
+    log.warn('signature absent ->'+ signature);
+    master.sendResponse(req, res, 403, 1, "Mandatory field not found");
+    return;
+  } else {
+    next();
+  }
+}
+
+var checkFlag = function(req, res, next) {
+  var flag = req.query.flag || req.body.flag;
+  log.info('flag middleware check ->'+ flag);
+
+  if(!(master.validateParameter(flag, 'flag'))) {
+    log.warn('flag absent->'+ flag);
+		master.sendResponse(req, res, 403, 1, "Mandatory field not found");
+		return;
+	} else {
+    next();
+  }
+}
+
+var checkEmail = function(req, res, next) {
+  var email = req.body.email;
+  log.info('email middleware check ->'+ email);
+
+  if (!(master.validateParameter(email, 'Email'))) {
+    log.warn('mail absent->'+ email);
+		master.sendResponse(req, res, 403, 1, "Mandatory field not found");
+		return;
+	} else if (!(helpers.validateEmail(email))) {
+    log.warn('Incorrect Email Format');
+		master.sendResponse(req, res, 200, 7, "Incorrect email id format");
+		return;
+  } else {
+    next();
+  }
+}
+
+module.exports = {
+  checkPubSignKey: checkPubSignKey,
+  checkFlag: checkFlag,
+  checkEmail: checkEmail
+};
