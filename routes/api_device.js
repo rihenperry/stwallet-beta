8e25a7c571188d7cfc400c2525be69590c2bd18f
rihbyne <rihen234@gmail.com>
2016-05-20 19:00:34 +0530

    check for req.body.token in getPvtKey

--- routes/api_device.js
+++ routes/api_device.js
@@ -4,7 +4,13 @@ var router = express.Router()
 var mw = require('../config/middleware');
 var device = require('../api/device');
 
-router.use(mw.checkPubSignKey);
+router.use(function(req, res, next) {
+  if (req.body.token && ((req.body.token === true) || (req.body.token === 'true'))) {
+    next()
+  } else {
+    mw.checkPubSignKey(req, res, next)
+  }
+})
 
 router.post('/register', device.deviceRegister);	// Device Register API
 router.post('/getPvtKey', device.getPvtKey);      // Get Private Key API
